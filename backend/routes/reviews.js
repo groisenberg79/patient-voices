ab254bca9fd47782e6e5953c0cca07543ef8e689
gabriel <groisenberg79@gmail.com>
2025-05-13 15:27:42 +0300

    add review fetching on frontend

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -12,6 +12,6 @@ router.post("/", authenticateToken, submitReview);
 // Fetch reviews for a disease by API ID
 router.get("/:api_id", fetchReviews);
 
-router.get("/disease/:id", getReviewsForDisease);
+router.get("/disease/:id", fetchReviews);
 
 module.exports = router;
