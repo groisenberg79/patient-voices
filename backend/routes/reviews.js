5eab6b02dc1c0b0a7920055e640f1d145f34b6d6
gabriel <groisenberg79@gmail.com>
2025-05-13 19:10:39 +0300

    add simple login page

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -12,6 +12,6 @@ router.post("/", authenticateToken, submitReview);
 // Fetch reviews for a disease by API ID
 router.get("/:api_id", fetchReviews);
 
-router.get("/disease/:id", fetchReviews);
+router.get("/disease/:api_id", fetchReviews);
 
 module.exports = router;
