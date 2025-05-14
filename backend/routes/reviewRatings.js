844fcf2f02b72d1d258484e28c7b239e5f4a4865
gabriel <groisenberg79@gmail.com>
2025-05-14 19:14:13 +0300

    add review rating in backend

--- backend/routes/reviewRatings.js
+++ backend/routes/reviewRatings.js
@@ -4,9 +4,9 @@ const {
   rateReview,
   getReviewRating,
 } = require("../controllers/reviewRatingController");
-const authenticateToken = require("../middleware/auth");
+const { authenticateToken } = require("../middleware/auth");
 
-router.post("/:review_id", authenticateToken, (req, res, next) => rateReview(req, res).catch(next));
+router.post("/:review_id", authenticateToken, rateReview);
 router.get("/:review_id", getReviewRating);
 
 module.exports = router;
